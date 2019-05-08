6a7d45b7dbd5c2cebc1b1fc10fd671cb62b34632
Valeeva_AR <adelina_chelny94@mail.ru>
2019-05-08 16:58:27 +0300

    08.05.19

--- js/script.js
+++ js/script.js
@@ -4,3 +4,16 @@ $( document ).ready(function() {
         $(this).siblings().children(".label").removeClass("label--active");
    });
 });
+
+var btn = document.querySelector('.ham-btn');
+var nav = document.querySelector('.nav');
+var btnTwo = document.querySelector('.cross_btn');
+btn.addEventListener('click', function(){
+        nav.classList.add('nav--mobile');
+        btnTwo.classList.add('cross_btn--active');
+});
+btnTwo.addEventListener('click', function(){
+        nav.classList.remove('nav--mobile');
+        btnTwo.classList.remove('cross_btn--active');
+
+});
